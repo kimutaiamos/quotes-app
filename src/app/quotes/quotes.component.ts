2d89a06662ad49c77329f4f0cbee92b94e79599e
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-09 23:44:36 +0300

    updated the quotes section

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,17 +9,17 @@ export class QuotesComponent implements OnInit {
 
   quote: Quotes[]=[
     new Quotes(
-      'love',
-      'John Green',
-      'As he read, I fell in love the way you fall asleep: slowly, and then all at once.',
-      'it tells more of the love life',
+      'happines',
+      'Mahatma ghandhi',
+      'happiness , is when waht you think ,what you say and what you think are in harmony:.',
+      'it tells more of the happy life',
       new Date(2000,3,10)
     ),
     new Quotes(
       'war',
-      'anonymous',
-      'Our Generation has had no Great war, no Great Depression. Our war is spiritual. Our depression is our lives.',
-      'tels of the strugles we go through',
+      'Thich Nhat Hanh',
+      'The present moment is filled with joy and happiness.Action may not always bring happiness, but there is no happiness without action. If you are attentive, you will see it.',
+      'tells of the strugles we go through',
       new Date(2021,3,4)
     ),
   ];
@@ -28,7 +28,6 @@ export class QuotesComponent implements OnInit {
     quote.datepublished= new Date(quote.completeDate)
     this.quote.push(quote);
   }
-  
 
   toggle(index){
     this.quote[index].showDetails = !this.quote[index].showDetails;
